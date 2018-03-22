859f4adabbab26fdc88981f73b88661a1093af20
Nitram96 <wolhert@gmail.com>
2018-03-22 09:17:18 +0100

    Starting project

---
+++ Battleships/gameSource/gameSource/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * gameSource.cpp
+ *
+ * Created: 22-Mar-18 09:14:08
+ * Author : martin
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
